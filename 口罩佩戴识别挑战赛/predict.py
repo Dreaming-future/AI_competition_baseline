cf4e30039707711f392d45cbfd9923098d0a788a
Dreaming-future <1016617094@qq.com>
2022-07-04 00:36:27 +0800

    规范化代码，并且增加概率输出

--- 口罩佩戴识别挑战赛/predict.py
+++ 口罩佩戴识别挑战赛/predict.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import torch
 import torch.nn.functional as F
+from tqdm import tqdm
 
 class Prediction():
     def __init__(self):
@@ -22,6 +23,7 @@ class Prediction():
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.num_classes = 3
         self.classes = ['mask_weared_incorrect', 'with_mask', 'without_mask']
+        self.Ensemble = False
 
     def load_model(self, net = 'ConvNeXt-B'):
         '''
@@ -46,11 +48,24 @@ class Prediction():
             self.net = DenseNet169(self.num_classes)
         elif net == 'DenseNet201':
             self.net = DenseNet201(self.num_classes)
-        
+        elif net == 'ViT-L':
+            from nets.ViT import Vit_large_patch16_224
+            self.net = Vit_large_patch16_224(num_classes)
+        elif net == 'ViT-H':
+            from nets.ViT import Vit_huge_patch14_224
+            self.net = Vit_huge_patch14_224(num_classes)
+        elif net == 'Swin-L':
+            from nets.Swin import swin_large_patch4_window7_224
+            self.net = swin_large_patch4_window7_224(num_classes)
             
+        checkpoint = torch.load('./checkpoint/{}_ckpt.pth'.format(net))
+        print('训练时，一共迭代了{}次，最后一次的准确率大概是 {} %'.format(checkpoint['epoch'],checkpoint['acc']))
+
         checkpoint = torch.load('./checkpoint/best_{}_ckpt.pth'.format(net))
         self.net.load_state_dict(checkpoint['net'])
         print('训练时，最佳的准确率的结果为 {} %'.format(checkpoint['acc']))
+        if self.use_gpu:
+            self.net = self.net.cuda()
         self.net.eval()
 
     def predict(self, image_path):
@@ -66,16 +81,17 @@ class Prediction():
             img = self.transform(img)
 
         if self.use_gpu:
-            self.net = self.net.cuda()
+        
             out = self.net(img.unsqueeze(dim=0).cuda())
             pred_label = np.argmax(out.cpu().detach().numpy())
-            # print(F.softmax(out)) # 得到概率
+            prob = F.softmax(out,dim=1).cpu().detach().numpy()
         else:
             out = self.net(img.unsqueeze(dim=0))
             pred_label = np.argmax(out.detach().numpy())
+            prob = F.softmax(out,dim=1).detach().numpy()
 
         # print("label:", pred_label)
-        return {"label": self.classes[pred_label]}
+        return {"label": self.classes[pred_label],"label_prob":prob[0]}
 
 
 def save_csv(path = 'submit.csv',net = 'ConvNeXt-B'):
@@ -84,20 +100,22 @@ def save_csv(path = 'submit.csv',net = 'ConvNeXt-B'):
 
     pred = Prediction()
     pred.load_model(net)
-    print("==> 开始测试文件中的图片,一共有{}张图片需要测试".format(len(test)))
-    for i,img_path in enumerate(test['path']):
-        pre = pred.predict(image_path=root + img_path)
-        test.iloc[i][1] = pre['label'] # 写入标签
-        if (i+1)%100 == 0:
-            print("已测试完毕{}张图片".format(i+1))
+    total = len(test)
+    print("==> 开始测试文件中的图片,一共有{}张图片需要测试".format(total))
+    with tqdm(total=total,desc=f'Predict Pictures {total}',mininterval=0.3) as pbar:
+        for i,img_path in enumerate(test['path']):
+            pre = pred.predict(image_path=root + img_path)
+            test.iloc[i][1] = pre['label'] # 写入标签
+            pbar.update(1)
+            # if (i+1)%100 == 0:
+            #     print("已测试完毕 {} 张图片".format(i+1))
     print("==> 测试完毕，正在保存文件 {}".format(path))
     test.to_csv( path, index=False)
     
     
 if __name__ == '__main__':
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    # os.environ['CUDA_VISIBLE_DEVICES'] = '0'
     root = './data/test//' # 文件夹的路径
     num_classes = 3 # 类别
-
-    net = 'ConvNeXt-XL'
+    net = 'ConvNeXt-T'
     save_csv(path = 'submit_{}.csv'.format(net),net = net)
