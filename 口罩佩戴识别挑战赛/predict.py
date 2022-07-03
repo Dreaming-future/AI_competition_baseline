d56037782b687ca9a05caf6abad8edc38a63e471
Dreaming-future <1016617094@qq.com>
2022-07-03 12:12:57 +0800

    规范predict.py

--- 口罩佩戴识别挑战赛/predict.py
+++ 口罩佩戴识别挑战赛/predict.py
@@ -1,48 +1,54 @@
-
-from nets.ConvNeXt import convnext_base
 import torchvision.transforms as transforms
 from PIL import Image
 import numpy as np
 import os
 import torch
-
-
+import torch.nn.functional as F
 
 class Prediction():
     def __init__(self):
         super(Prediction, self).__init__()
+        # 利用ImageNet的标准差和方差
         image_mean = [0.38753143, 0.36847523, 0.27735737]
         image_std = [0.2023, 0.1994, 0.2010]
+        # 不进行数据增强的transforms
         self.transform = transforms.Compose([
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize(image_mean, image_std)
         ])
+        
         self.use_gpu = torch.cuda.is_available()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.num_classes = 3
         self.classes = ['mask_weared_incorrect', 'with_mask', 'without_mask']
 
-    def load_model(self):
+    def load_model(self, net = 'ConvNeXt-B'):
         '''
         模型初始化，必须在此方法中加载模型
         '''
         print('==> Resuming from checkpoint..')
         assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-
-        net_type = 'ConvNeXt-XL'
         
-        # from nets.DenseNet import DenseNet201,DenseNet169
-        # self.net = DenseNet201(3)
         from nets.ConvNeXt import convnext_tiny,convnext_base,convnext_large,convnext_xlarge
-        if net_type == 'ConvNeXt-T':
-            self.net = convnext_tiny(3)
-        elif net_type == 'ConvNeXt-B':
-            self.net = convnext_base(3)
-        elif net_type == 'ConvNeXt-L':
-            self.net = convnext_large(3)
-        elif net_type == 'ConvNeXt-XL':
-            self.net = convnext_xlarge(3)
-        checkpoint = torch.load('./checkpoint/best_{}_ckpt.pth'.format(net_type))
+        from nets.DenseNet import DenseNet201,DenseNet169
+
+        print("我们使用的是 {} 模型进行测试".format(net))
+        if net == 'ConvNeXt-T':
+            self.net = convnext_tiny(self.num_classes)
+        elif net == 'ConvNeXt-B':
+            self.net = convnext_base(self.num_classes)
+        elif net == 'ConvNeXt-L':
+            self.net = convnext_large(self.num_classes)
+        elif net == 'ConvNeXt-XL':
+            self.net = convnext_xlarge(self.num_classes)
+        elif net == 'DenseNet169':
+            self.net = DenseNet169(self.num_classes)
+        elif net == 'DenseNet201':
+            self.net = DenseNet201(self.num_classes)
+        
+            
+        checkpoint = torch.load('./checkpoint/best_{}_ckpt.pth'.format(net))
         self.net.load_state_dict(checkpoint['net'])
         print('训练时，最佳的准确率的结果为 {} %'.format(checkpoint['acc']))
         self.net.eval()
@@ -63,6 +69,7 @@ class Prediction():
             self.net = self.net.cuda()
             out = self.net(img.unsqueeze(dim=0).cuda())
             pred_label = np.argmax(out.cpu().detach().numpy())
+            # print(F.softmax(out)) # 得到概率
         else:
             out = self.net(img.unsqueeze(dim=0))
             pred_label = np.argmax(out.detach().numpy())
@@ -70,19 +77,27 @@ class Prediction():
         # print("label:", pred_label)
         return {"label": self.classes[pred_label]}
 
-import pandas as pd
-test = pd.read_csv('./data/sample_submit.csv')
 
-root = './data/test//'
-num_classes = 3
-imgs = os.listdir('./data/test/')
+def save_csv(path = 'submit.csv',net = 'ConvNeXt-B'):
+    import pandas as pd
+    test = pd.read_csv('./data/sample_submit.csv')
 
-pred = Prediction()
-pred.load_model()
-pred.predict(image_path=root + imgs[1])
-for i,img_path in enumerate(test['path']):
-    # print(img_path)
-    pre = pred.predict(image_path=root + img_path)
-    test.iloc[i][1] = pre['label']
+    pred = Prediction()
+    pred.load_model(net)
+    print("==> 开始测试文件中的图片,一共有{}张图片需要测试".format(len(test)))
+    for i,img_path in enumerate(test['path']):
+        pre = pred.predict(image_path=root + img_path)
+        test.iloc[i][1] = pre['label'] # 写入标签
+        if (i+1)%100 == 0:
+            print("已测试完毕{}张图片".format(i+1))
+    print("==> 测试完毕，正在保存文件 {}".format(path))
+    test.to_csv( path, index=False)
+    
+    
+if __name__ == '__main__':
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    root = './data/test//' # 文件夹的路径
+    num_classes = 3 # 类别
 
-test.to_csv('submit.csv',index=False)
+    net = 'ConvNeXt-L'
+    save_csv(path = 'submit_{}.csv'.format(net),net = net)
