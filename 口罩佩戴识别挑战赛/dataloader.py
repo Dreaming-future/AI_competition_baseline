750534123e86a1508341b67e6cedfec4ce1286c4
Dreaming-future <1016617094@qq.com>
2022-07-11 10:45:01 +0800

    增加数据文件夹的参数

--- 口罩佩戴识别挑战赛/dataloader.py
+++ 口罩佩戴识别挑战赛/dataloader.py
@@ -7,7 +7,7 @@ image_mean = [0.4940, 0.4187, 0.3855]
 image_std = [0.2048, 0.1941, 0.1932]
 
 # Data
-def get_training_dataloader(batch_size = 64, num_workers = 4, shuffle = True, resize = 224):
+def get_training_dataloader(batch_size = 64, num_workers = 4, shuffle = True, resize = 224, root = './data//'):
     print('==> Preparing Train data..')
     transform_train = transforms.Compose([
         transforms.Resize(resize),
@@ -16,16 +16,15 @@ def get_training_dataloader(batch_size = 64, num_workers = 4, shuffle = True, re
         transforms.ToTensor(),
         transforms.Normalize(image_mean, image_std),
     ])
-    root = './data//'
     trainset = torchvision.datasets.ImageFolder(root + 'train',transform=transform_train)
-    # classes = trainset.classes
-    # print("分类类别",classes)
+    classes = trainset.classes
+    print("==> 分类类别", " ".join(classes))
     # print(trainset)
     trainloader = torch.utils.data.DataLoader(
         trainset, batch_size=batch_size, shuffle=shuffle, num_workers= num_workers, pin_memory = True)
     return trainloader
     
-def get_test_dataloader(batch_size = 64, num_workers = 4, shuffle = True, resize = 224): 
+def get_test_dataloader(batch_size = 64, num_workers = 4, shuffle = True, resize = 224, root = './data//'): 
     print('==> Preparing Test data..')   
     transform_test = transforms.Compose([
         transforms.Resize(resize),
