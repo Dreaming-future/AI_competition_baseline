9273e99ec156fe7cc8ff042afb2218b8a57cd9c0
Dreaming-future <1016617094@qq.com>
2022-07-15 14:48:15 +0800

    快速训练以及预测方法

---
+++ LED灯的色彩和均匀性检测挑战赛/predict_ensemble.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=0 python predict.py -t mean -td 99.8
+CUDA_VISIBLE_DEVICES=0 python predict.py -t mean -td 99.5
+CUDA_VISIBLE_DEVICES=0 python predict.py -t mean -td 99.0
+CUDA_VISIBLE_DEVICES=0 python predict.py -t vote
